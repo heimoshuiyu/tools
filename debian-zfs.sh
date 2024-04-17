6a6552677359096d9c47f95599150e6abbdafd8a
黑墨水鱼 <heimoshuiyu@gmail.com>
2024-04-17 11:30:23 +0800

    更新 debian-zfs.sh

--- debian-zfs.sh
+++ debian-zfs.sh
@@ -1,3 +1,3 @@
 codename=$(lsb_release -cs);echo "deb http://deb.debian.org/debian $codename-backports main contrib non-free"|sudo tee -a /etc/apt/sources.list && sudo apt update
 sudo apt install -y linux-headers-amd64
-sudo apt install -t stable-backports zfsutils-linux
+sudo apt install -y -t stable-backports zfsutils-linux
